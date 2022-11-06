b4fd7c0a2f97f276fa2166ec94fe0ce5a3732518
Ashley Evans <ashleyev@hotmail.co.uk>
2022-11-06 17:37:10 +0000

    Update dig.lua to dig up and down on each movement

--- dig.lua
+++ dig.lua
@@ -4,6 +4,16 @@
 -- avoids actually digging anything useful? (e.g. diamonds)
 -- for now pauses at this point
     -- later works around the deposit and carries on digging the tunnel
+SLOTS = {
+    FUEL = 1
+    TORCH = 2
+}
+
+DIRECTIONS = {
+    FORWARD = "forward"
+    UP = "up"
+    DOWN = "down"
+}
 
 function tableContains(table, val)
     for i, v in ipairs(table) do
@@ -15,33 +25,49 @@ function tableContains(table, val)
 end
 
 function blockIsResource(blockToCheck)
-    local resourceBlocks = {"minecraft:diorite", "minecraft:jungle_planks"}
+    local resourceBlocks = {
+        "minecraft:diamond_ore",
+        "minecraft:redstone_ore",
+        "minecraft:gold_ore",
+        "minecraft:iron_ore",
+        "minecraft:lapis_ore",
+        "minecraft:emerald_ore",
+        "minecraft:copper_ore"
+    }
     return tableContains(resourceBlocks, blockToCheck)
 end
 
-function digIfSafe() 
-    local hasBlock, data = turtle.inspect()
-    if blockIsResource(data.name) then
-        return false
+function digIfSafe(direction)
+    if direction == DIRECTIONS.FORWARD
+        local hasBlock, data = turtle.inspect()
+        if not blockIsResource(data.name) then
+            turtle.dig()
+            return true
+        end
+    elseif direction == DIRECTIONS.UP
+        local hasBlock, data = turtle.inspectUp()
+        if not blockIsResource(data.name) then
+            turtle.digUp()
+            return true
+        end
+    elseif direction == DIRECTIONS.DOWN
+        local hasBlock, data = turtle.inspectDown()
+        if not blockIsResource(data.name) then
+            turtle.digDown()
+            return true
+        end
     else
-        turtle.dig()
-        return true
+        print("invalid direction given")
     end
+
+    return false
 end
 
-function digUpIfSafe()
-    local hasBlock, data = turtle.inspectUp()
-    if blockIsResource(data.name) then
-        return false
-    else
-        turtle.digUp()
-        return true
-    end
-end
 
 function refuelIfBelow(fuelLimit)
     if turtle.getFuelLevel() < fuelLimit then
         print("refueling")
+        turtle.select(SLOTS.FUEL)
         success = turtle.refuel()
         if not success then
             print("failed to refuel")
@@ -49,6 +75,8 @@ function refuelIfBelow(fuelLimit)
         end
     end
 end
+
+
 
 function dig()
     local count = 0
@@ -59,17 +87,20 @@ function dig()
 
         refuelIfBelow(2)
 
-        local dug = digIfSafe()
+        local dug = digIfSafe(DIRECTIONS.FORWARD)
         if not dug then
-            print("dig not safe")
             return
         end
 
         turtle.forward()
 
-        dug = digUpIfSafe()
+        dug = digIfSafe(DIRECTIONS.UP)
         if not dug then
-            print("dig up not safe")
+            return
+        end
+
+        dug = digIfSafe(DIRECTIONS.DOWN)
+        if not dug then
             return
         end
 
@@ -77,4 +108,15 @@ function dig()
     end
 end
 
-dig()
+function startUp()
+    -- check fuel level
+    turtle.select(SLOTS.FUEL)
+    fuel = turtle.refuel(0)
+    if not fuel then
+        print("no fuel found in fuel slot: " .. SLOTS.FUEL)
+    end
+
+    dig()
+end
+
+startUp()
