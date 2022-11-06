360745cbc3f4308b29eadc0e66f3a4c7e5467bf1
Ashley Evans <ashleyev@hotmail.co.uk>
2022-11-06 17:47:53 +0000

    Update dig.lua to cover all minecraft ores in whitelist

--- dig.lua
+++ dig.lua
@@ -32,7 +32,16 @@ function blockIsResource(blockToCheck)
         "minecraft:iron_ore",
         "minecraft:lapis_ore",
         "minecraft:emerald_ore",
-        "minecraft:copper_ore"
+        "minecraft:copper_ore",
+        "minecraft:deepslate_diamond_ore",
+        "minecraft:deepslate_redstone_ore",
+        "minecraft:deepslate_gold_ore",
+        "minecraft:deepslate_iron_ore",
+        "minecraft:deepslate_lapis_ore",
+        "minecraft:deepslate_emerald_ore",
+        "minecraft:deepslate_copper_ore",
+        "minecraft:nether_gold_ore",
+        "minecraft:ancientDebris"
     }
     return tableContains(resourceBlocks, blockToCheck)
 end
