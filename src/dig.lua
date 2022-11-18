96a729fa7a12ee015689381aa2574fe0521a424f
Ashley Evans <ashleyev@hotmail.co.uk>
2022-11-18 21:57:02 +0000

    Refactor mine pattern into collections

--- src/dig.lua
+++ src/dig.lua
@@ -6,14 +6,8 @@ local function dig()
     local state = t.createState()
     local collectionArgs = {
         times = 10,
-        actions = {
-            {run = a.collection, args = { times = 10, actions = c.DIG_MOVE_UP_DOWN }},
-            {run = a.collection, args = { times = 1, actions = c.U_TURN_RIGHT_3 }},
-            {run = a.collection, args = { times = 10, actions = c.DIG_MOVE_UP_DOWN}},
-            {run = a.collection, args = { times = 1, actions = c.U_TURN_LEFT_3 }},
-        }
+        actions = c.minePattern(10, 2)
     }
-
     a.collection(state, collectionArgs)
 end
 
