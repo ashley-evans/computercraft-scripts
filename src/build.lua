a319a2a751c2c982d28b91ba635d883bda6a1ed7
Ben Currer-Burgess <bencurrer@gmail.com>
2022-11-22 22:25:07 +0000

    make final files easier to use

--- src/build.lua
+++ src/build.lua
@@ -2,15 +2,14 @@ local t = require("turtle-utils")
 local a = require("actions")
 local c = require("collections")
 
-local forward = 8
-local right = 4
-local height = 2
+local block = "deepslate_bricks"
+local length = 64
 
 local function build()
     local state = t.createState()
     local collectionArgs = {
         times = 1,
-        actions = c.floorSection(forward, "deepslate_bricks")
+        actions = c.floorSection(length, block)
     }
     a.safeCollection(state, collectionArgs)
 end
--- src/dig.lua
+++ src/dig.lua
@@ -1,14 +1,13 @@
-local t = require("turtle-utils")
-local a = require("actions")
-local c = require("collections")
-
+local t = require("turtle-utils") local a = require("actions") local c = require("collections")
+local length = 10
+--local gap = 2
+--local halfWidth = 5
 local function dig()
     local state = t.createState()
     local collectionArgs = {
-        times = 10,
-        actions = c.minePattern(10, 2)
+        times = length,
+        actions = c.DIG_MOVE_UP_DOWN
     }
     a.safeCollection(state, collectionArgs)
 end
-
 dig()
